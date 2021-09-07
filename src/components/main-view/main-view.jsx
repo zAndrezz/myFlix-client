eb0d2f5d62a1321bbfd39a1482698cce1efc046b
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-09-06 20:13:58 -0500

    movie card and movie view component created

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+class MainView extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
+        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+      ],
+      selectedMovie: null
+    };
+  }
+
+ render() {
+  const { movies, selectedMovie } = this.state;
+
+  if (movies.length === 0) return <div className="main-view" />;
+
+  return (
+    <div className="main-view">
+      {selectedMovie
+        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+        : movies.map(movie => (
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+        ))
+      }
+    </div>
+  );
+}
+
+}
+
+
+
+
+
---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import "./movie-view.scss";
+
+export class MovieView extends React.Component {
+
+  
+
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="movie-actors">
+          <span className="label">Actors: </span>
+          <span className="value">{movie.Actors.map(actor => actor.Name).join(", ")}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+
+      </div>
+    );
+  }
+}
+
