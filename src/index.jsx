b124791f1080cfbb56f6ced50bf2bf1ca6f4fe6b
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-10-12 12:56:29 -0500

    implementing redux to project

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< Updated upstream
-import MainView from './components/main-view/main-view';
-
-// Import statement to indicate that you need to bundle `./index.scss`
-=======
 import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -15,7 +10,6 @@ import  MainView  from './components/main-view/main-view';
 
 
 // Import statement to indicate that we need to bundle `./index.scss`
->>>>>>> Stashed changes
 import './index.scss';
 
 const store = createStore(moviesApp, devToolsEnhancer());
@@ -23,17 +17,12 @@ const store = createStore(moviesApp, devToolsEnhancer());
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-<<<<<<< Updated upstream
-    return (
-      <MainView />
-=======
     return(
      <Provider store={store}>
         <Container>
           <MainView />
         </Container>
       </Provider>
->>>>>>> Stashed changes
     );
   }
 }
@@ -41,9 +30,5 @@ class MyFlixApplication extends React.Component {
 // Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
 
-<<<<<<< Updated upstream
-// Tells React to render your app in the root DOM element
-=======
 // Tell React to render our app in the root DOM element
->>>>>>> Stashed changes
 ReactDOM.render(React.createElement(MyFlixApplication), container);
